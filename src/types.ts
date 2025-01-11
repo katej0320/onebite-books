819935f2e1f974c3d7ccbc1483373d4bb784d1d3
Kate <Kate@JHsui-MacBookAir.local>
2025-01-11 16:00:04 +0900

    worked on UI

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+
+
+export interface BookData {
+    id : number;
+    title : string;
+    subTitle: string;
+    author :string;
+    publisher: string;
+    description: string;
+    coverImgUrl:  string;
+}
